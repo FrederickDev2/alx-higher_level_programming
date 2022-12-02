5a7a244cf3850c00c8b24c186b998daebe8507f0
Frederick Afriyie <frederick.afriyie@upt.com.gh>
2022-12-01 16:29:33 -0800

    100  error fixed

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #!/usr/bin/python3
 """ Write a program that imports all functions from
 the file calculator_1.py and handles basic operations.
@@ -44,29 +43,3 @@ if __name__ == "__main__":
     else:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
-=======
-#!/usr/bin/python3
-
-
-if __name__ == '__main__':
-    import sys
-    from calculator_1 import add, sub, mul, div
-
-    if len(sys.argv) != 4:
-        print('Usage: {:s} <a> <operator> <b>'.format(sys.argv[0]))
-        sys.exit(1)
-    if sys.argv[2] not in ['+', '-', '*', '/']:
-        print('Unknown operator. Available operators: +, -, * and /')
-        sys.exit(1)
-
-    a = int(sys.argv[1])
-    b = int(sys.argv[3])
-    if sys.argv[2] == '+':
-        print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
-    elif sys.argv[2] == '-':
-        print('{:d} - {:d} = {:d}'.format(a, b, sub(a, b)))
-    elif sys.argv[2] == '*':
-        print('{:d} * {:d} = {:d}'.format(a, b, mul(a, b)))
-    else:
-        print('{:d} / {:d} = {:d}'.format(a, b, div(a, b)))
->>>>>>> c2505d1cad6bf7a56dbe6b724df65584286d18a6
